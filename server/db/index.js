00c07ca24847c7d8010c6d92c34375418f586678
zktnguyen <zktnguyen@ucdavis.edu>
2017-12-22 23:02:33 -0800

    added test database

--- server/db/index.js
+++ server/db/index.js
@@ -2,8 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-const mongoURI = process.env.MONGODB_URI || 
-  'mongodb://localhost:27017/TodoApp';
+const mongoURI = process.env.MONGODB_URI;
 mongoose.connect(mongoURI, { useMongoClient: true });
 
 module.exports = { mongoose };
--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('./config');
 var express = require('express');
 var bodyParser = require('body-parser');
 var { ObjectID } = require('mongodb');
