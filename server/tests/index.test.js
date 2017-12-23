fdb6134103de92e7872c94f397eeed3924329174
zktnguyen <zktnguyen@ucdavis.edu>
2017-12-22 17:18:20 -0800

    Tests for GET /todos route written

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -4,6 +4,14 @@ const request = require('supertest');
 const { app } = require('../');
 const { Todo } = require('../models/todo');
 
+const seedTodos = [{ text: 'first test todo' }, { text: 'second test todo'}];
+
+beforeEach(done => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(seedTodos);
+  }).then(() => done());
+});
+
 describe('POST /todos', () => {
   it('should create a new todo', done => {
     let text = 'Test todo text';
@@ -19,7 +27,7 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then(todos => {
+        Todo.find({text}).then(todos => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -35,9 +43,21 @@ describe('POST /todos', () => {
       .end((err, res) => {
         if (err) { return done(err); }
         Todo.find().then(todos => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch(e => done(e));
       });
   });
 });
+
+describe('GET /todos', () => {
+  it('should get all the todos', done => {
+    request(app)
+      .get('/todos')
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todos.length).toBe(2);
+      })
+      .end(done);
+  })
+})
