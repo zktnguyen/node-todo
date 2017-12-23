cb2890ee448c8eb431ee1a154a692e5f97f77e41
zktnguyen <zktnguyen@ucdavis.edu>
2017-12-22 21:17:47 -0800

    tests for DELETE /todos/:id route done

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -96,3 +96,39 @@ describe('GET /todos/:id', () => {
   });
 });
 
+describe('DELETE /todos/:id', () => {
+  it('should remove todo with id', done => {
+    let hexId = seedTodos[1]._id.toHexString();
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo) => {
+          expect(todo).toBe(null);
+          done();
+        }).catch(e => done(e));
+      });
+  });
+  
+  it('should return 404 if todo is not found', done => {
+    request(app)
+      .delete(`/todos/${(new ObjectID()).toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 for valid object id', done => {
+    request(app)
+      .delete('/todos/123')
+      .expect(404)
+      .end(done);
+  });
+});
+
