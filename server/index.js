8fe0ce811c3ea2219ae8727ba3394ec7e8640215
zktnguyen <zktnguyen@ucdavis.edu>
2017-12-22 21:04:45 -0800

    DELETE /todos/:id routes created

--- server/index.js
+++ server/index.js
@@ -46,6 +46,22 @@ app.get('/todos/:id', (req, res) => {
   }).catch(e => res.status(400).send());
 });
 
+app.delete('/todos/:id', (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findByIdAndRemove(id).then(todo => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({ todo });
+  }).catch(e => res.status(400).send());
+});
+
+
 app.listen(port, () => console.log('Started on port 3000'));
 
 module.exports = { app };
