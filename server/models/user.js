331d30cf6d28e3c8328321991849ccb5645134f3
zktnguyen <zktnguyen@ucdavis.edu>
2017-12-23 16:49:39 -0800

    Created POST /users/login route to allow for logging in users

--- server/models/user.js
+++ server/models/user.js
@@ -74,6 +74,26 @@ userSchema.statics.findByToken = function (token) {
   });
 };
 
+userSchema.statics.findByCredentials = function(email, password) {
+  let User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 userSchema.pre('save', function(next) {
   let user = this;
 
@@ -88,6 +108,7 @@ userSchema.pre('save', function(next) {
     next();
   }
 });
+
 
 var User = mongoose.model('User', userSchema);
 module.exports = { User };
